3f226d6c4fa1c5abbecb0dddc6b92b2a6370c18b
danielweck <daniel.weck@gmail.com>
2018-03-28 15:37:40 +0100

    added license header in TypeScript source, see https://github.com/edrlab/readium-desktop/issues/105

--- src/electron/common/sessions.ts
+++ src/electron/common/sessions.ts
@@ -1,3 +1,10 @@
+// ==LICENSE-BEGIN==
+// Copyright 2017 European Digital Reading Lab. All rights reserved.
+// Licensed to the Readium Foundation under one or more contributor license agreements.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file exposed on Github (readium) in the project repository.
+// ==LICENSE-END==
+
 export const R2_SESSION_WEBVIEW = "persist:readium2pubwebview";
 
 export const READIUM2_ELECTRON_HTTP_PROTOCOL = "httpsr2";
