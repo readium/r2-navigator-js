2ff7e3740f807f579de8d1020e5937392c8566a5
Daniel Weck <daniel.weck@gmail.com>
2019-04-17 10:25:04 +0100

    LCP LSD now proper TypeScript model (remove 'any' type)

--- src/electron/main/lsd.ts
+++ src/electron/main/lsd.ts
@@ -8,52 +8,77 @@
 import { IDeviceIDManager } from "@r2-lcp-js/lsd/deviceid-manager";
 import { lsdRenew } from "@r2-lcp-js/lsd/renew";
 import { lsdReturn } from "@r2-lcp-js/lsd/return";
+import { LSD } from "@r2-lcp-js/parser/epub/lsd";
 import { Server } from "@r2-streamer-js/http/server";
 import * as debug_ from "debug";
 import * as moment from "moment";
-
+import { JSON as TAJSON } from "ta-json-x";
 const debug = debug_("r2:navigator#electron/main/lsd");
+
+const IS_DEV = (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "dev");
 
 export async function doLsdReturn(
     publicationsServer: Server,
     deviceIDManager: IDeviceIDManager,
-    publicationFilePath: string): Promise<any> {
+    publicationFilePath: string): Promise<LSD> {
 
     const publication = publicationsServer.cachedPublication(publicationFilePath);
-    if (!publication || !publication.LCP || !publication.LCP.LSDJson) {
+    if (!publication || !publication.LCP || !publication.LCP.LSD) {
         return Promise.reject("no publication LCP LSD data?!");
     }
 
-    let renewResponseJson: any;
+    let returnResponseJson: any;
     try {
-        renewResponseJson = await lsdReturn(publication.LCP.LSDJson, deviceIDManager);
-        publication.LCP.LSDJson = renewResponseJson;
-        return Promise.resolve(renewResponseJson);
+        returnResponseJson = await lsdReturn(publication.LCP, deviceIDManager);
     } catch (err) {
         debug(err);
         return Promise.reject(err);
     }
+    if (returnResponseJson) {
+        try {
+            publication.LCP.LSD = TAJSON.deserialize<LSD>(returnResponseJson, LSD);
+            if (IS_DEV) {
+                debug(publication.LCP.LSD);
+            }
+            return Promise.resolve(publication.LCP.LSD);
+        } catch (err) {
+            debug(err);
+            return Promise.reject(err);
+        }
+    }
+    return Promise.reject("doLsdReturn?!");
 }
 
 export async function doLsdRenew(
     publicationsServer: Server,
     deviceIDManager: IDeviceIDManager,
     publicationFilePath: string,
-    endDateStr: string | undefined): Promise<any> {
+    endDateStr: string | undefined): Promise<LSD> {
 
     const publication = publicationsServer.cachedPublication(publicationFilePath);
-    if (!publication || !publication.LCP || !publication.LCP.LSDJson) {
+    if (!publication || !publication.LCP || !publication.LCP.LSD) {
         return Promise.reject("no publication LCP LSD data?!");
     }
 
     const endDate = endDateStr ? moment(endDateStr).toDate() : undefined;
     let renewResponseJson: any;
     try {
-        renewResponseJson = await lsdRenew(endDate, publication.LCP.LSDJson, deviceIDManager);
-        publication.LCP.LSDJson = renewResponseJson;
-        return Promise.resolve(renewResponseJson);
+        renewResponseJson = await lsdRenew(endDate, publication.LCP, deviceIDManager);
     } catch (err) {
         debug(err);
         return Promise.reject(err);
     }
+    if (renewResponseJson) {
+        try {
+            publication.LCP.LSD = TAJSON.deserialize<LSD>(renewResponseJson, LSD);
+            if (IS_DEV) {
+                debug(publication.LCP.LSD);
+            }
+            return Promise.resolve(publication.LCP.LSD);
+        } catch (err) {
+            debug(err);
+            return Promise.reject(err);
+        }
+    }
+    return Promise.reject("doLsdRenew?!");
 }
