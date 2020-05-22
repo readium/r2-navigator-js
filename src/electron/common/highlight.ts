798572cc3dcc0330372be3dbe63cc1a465f6a565
Daniel Weck <daniel.weck@gmail.com>
2020-05-22 21:39:32 +0100

    added utility RGB function

--- src/electron/common/highlight.ts
+++ src/electron/common/highlight.ts
@@ -24,3 +24,16 @@ export interface IHighlightDefinition {
     selectionInfo: ISelectionInfo | undefined;
     color: IColor | undefined;
 }
+
+export function convertColorHexadecimalToRGBA(cssHex: string, alpha?: number): string | undefined {
+    if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(cssHex)) {
+        const hex = cssHex.substring(1);
+        const hex_ = hex.length === 3 ?
+            `0x${hex[0]}${hex[0]}${hex[1]}${hex[1]}${hex[2]}${hex[2]}` :
+            `0x${hex[0]}${hex[1]}${hex[2]}${hex[3]}${hex[4]}${hex[5]}`;
+        const hexVal = parseInt(hex_, 16);
+        // tslint:disable-next-line: no-bitwise
+        return `rgb${alpha ? "a" : ""}(${(hexVal >> 16) & 255}, ${(hexVal >> 8) & 255}, ${hexVal & 255}${alpha ? `, ${alpha}` : ""})`;
+    }
+    return undefined;
+}
